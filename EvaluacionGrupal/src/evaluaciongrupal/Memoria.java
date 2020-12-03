e4bbfeb1335c5251e96a4fdffcc185adc14c7fdb
Marlon Ramirez <Marlon Ramirez@Marlie>
2020-12-02 18:19:10 -0600

    Version 1.0

---
+++ EvaluacionGrupal/src/evaluaciongrupal/Memoria.java
@@ -0,0 +1,30 @@
+package evaluaciongrupal;
+
+
+/**
+ * @author Armandol
+ * @version 1.0
+ * @created 02-Dec-2020 6:14:00 PM
+ */
+public class Memoria {
+
+	private float m1;
+	private float m2;
+
+	public Memoria(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void clear(){
+
+	}
+
+	public void save(){
+
+	}
+
+}
---
+++ EvaluacionGrupal/src/evaluaciongrupal/UAL.java
@@ -0,0 +1,37 @@
+package evaluaciongrupal;
+
+
+/**
+ * @author Armandol
+ * @version 1.0
+ * @created 02-Dec-2020 6:14:02 PM
+ */
+public class UAL {
+
+	public Memoria m_Memoria;
+
+	public UAL(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public boolean igual(){
+		return false;
+	}
+
+	public boolean menor(){
+		return false;
+	}
+
+	public int multiplicar(){
+		return 0;
+	}
+
+	public int sumar(){
+		return 0;
+	}
+
+}
